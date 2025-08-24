ad9894a0103bf215f8f6cd1e41cbfbc9050991d9
lakhatekno <lakhatekno2514@gmail.com>
2025-08-24 15:13:05 +0700

    [UI][Toast] update init state toast

--- src/services/toast.store.ts
+++ src/services/toast.store.ts
@@ -2,10 +2,10 @@ import { Toast } from '@/types/toast.type';
 import { create } from 'zustand';
 
 export const useToast = create<Toast>((set) => ({
-  type: 'success',
-  title: 'Title',
-  message: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt sit dignissimos consectetur.',
-  openToast: true,
+  type: 'news',
+  title: '',
+  message: '',
+  openToast: false,
 
   setOpenToast: (props) =>
     set({
